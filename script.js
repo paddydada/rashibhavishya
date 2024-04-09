c42e9eabb295caed3610fe9227e66979d43a6880
pravin234 <pravinkadam234@gmail.com>
2024-04-09 23:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,203 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     flatpickr("#dates", {
-//         dateFormat: "Y-m-d", // Set the date format
-//         disableMobile: true // Disable mobile mode to ensure consistent behavior
-//     });
-  
-//   const fileDropdown = document.getElementById("files");
-//   const keysDropdown = document.getElementById("keys");
-//   const valuesDropdown = document.getElementById("values");
-//   const datesInput = document.getElementById("dates");
-
-//   // Retrieve selected file name from local storage
-//   const storedRashi = localStorage.getItem("selected_rashi");
-//   if (storedRashi) {
-//     fileDropdown.value = storedRashi;
-//   }
-
-//   fileDropdown.dispatchEvent(new Event("change"));
-
-//   fileDropdown.addEventListener("change", function (event) {
-//     const selectedFile = event.target.value;
-
-//     localStorage.setItem("selected_rashi", selectedFile);
-
-//     fetch(selectedFile)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Clear previous options
-//         keysDropdown.innerHTML = "";
-
-//         data.forEach((entry) => {
-//           const key = Object.keys(entry)[0];
-//           const option = document.createElement("option");
-//           option.text = key;
-//           option.value = key;
-//           keysDropdown.add(option);
-//         });
-
-//         keysDropdown.dispatchEvent(new Event("change"));
-//       })
-//       .catch((error) => console.error("Error loading JSON data:", error));
-//   });
-
-//   keysDropdown.addEventListener("change", function (event) {
-//     const selectedKey = event.target.value;
-//     const selectedFile = fileDropdown.value;
-
-//     fetch(selectedFile)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Find the entry with the selected key
-//         const selectedEntry = data.find(
-//           (entry) => Object.keys(entry)[0] === selectedKey
-//         );
-
-//         localStorage.setItem("selectedKey", selectedKey);
-//         localStorage.setItem(
-//           "selectedData",
-//           JSON.stringify(selectedEntry[selectedKey])
-//         );
-
-//         populateValuesDropdown(selectedEntry[selectedKey]);
-//       })
-//       .catch((error) => console.error("Error loading JSON data:", error));
-//   });
-
-//   function populateValuesDropdown(values) {
-//     valuesDropdown.innerHTML = "";
-
-//     for (const key in values) {
-//       const option = document.createElement("option");
-//       option.text = values[key];
-//       option.value = values[key];
-//       valuesDropdown.add(option);
-//     }
-//   }
-
-//   datesInput.addEventListener("change", function (event) {
-//     const selectedDate = new Date(event.target.value);
-//     const dayOfWeek = selectedDate.getDay();
-//     let selectedDayFile;
-
-//     switch (dayOfWeek) {
-//       case 0: // Sunday
-//         selectedDayFile = "sunday.json";
-//         break;
-//       case 1: // Monday
-//         selectedDayFile = "monday.json";
-//         break;
-//       case 2: // Tuesday
-//         selectedDayFile = "tuesday.json";
-//         break;
-//       case 3: // Wednesday
-//         selectedDayFile = "wednesday.json";
-//         break;
-//       case 4: // Thursday
-//         selectedDayFile = "thursday.json";
-//         break;
-//       case 5: // Friday
-//         selectedDayFile = "friday.json";
-//         break;
-//       case 6: // Saturday
-//         selectedDayFile = "saturday.json";
-//         break;
-//       default:
-//         console.error("Invalid day selected.");
-//         return;
-//     }
-
-//     fetch(selectedDayFile)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const selectedRashi = localStorage.getItem("selected_rashi");
-
-//         const rashiName = selectedRashi
-//           ? selectedRashi.split(".")[0].toUpperCase()
-//           : null;
-
-//         if (rashiName && data[rashiName + "PL"] && data[rashiName + "MN"]) {
-//           const rashiPl = rashiName + "PL";
-//           const rashiMn = rashiName + "MN";
-
-//           const rashiPlData = data[rashiPl];
-//           const rashiMnData = data[rashiMn];
-
-//           const nseValue = prompt("Enter NSE value:");
-//           const bseValue = prompt("Enter BSE value:");
-
-//           const rashiPlObject = createObjectWithNSEValues(
-//             rashiPlData,
-//             parseFloat(bseValue)
-//           );
-//           const rashiMnObject = createObjectWithBSEValues(
-//             rashiMnData,
-//             parseFloat(nseValue)
-//           );
-
-//           localStorage.setItem("rashiPlObject", JSON.stringify(rashiPlObject));
-//           localStorage.setItem("rashiMnObject", JSON.stringify(rashiMnObject));
-
-//           const rashiMnChartTitle = `${rashiName} Mn Chart (NSE)`;
-//           const rashiPlChartTitle = `${rashiName} Pl Chart (BSE)`;
-//           prepareChart("nseChart", rashiMnChartTitle, rashiMnObject);
-//           prepareChart("bseChart", rashiPlChartTitle, rashiPlObject);
-//         } else {
-//           console.error("Invalid or missing Rashi selected.");
-//         }
-//       })
-//       .catch((error) => console.error("Error loading JSON data:", error));
-//   });
-
-//   function createObjectWithBSEValues(data, nseInput) {
-//     const obj = {};
-//     for (const key in data) {
-//       obj[key] = data[key] + nseInput; // Add NSE input to existing BSE values
-//     }
-//     return obj;
-//   }
-
-//   function createObjectWithNSEValues(data, bseInput) {
-//     const obj = {};
-//     for (const key in data) {
-//       obj[key] = data[key] + bseInput; // Add BSE input to existing NSE values
-//     }
-//     return obj;
-//   }
-
-//   function prepareChart(containerId, title, data) {
-//     Highcharts.chart(containerId, {
-//       chart: {
-//         type: "column",
-//       },
-//       title: {
-//         text: title,
-//       },
-//       xAxis: {
-//         categories: Object.keys(data),
-//       },
-//       yAxis: {
-//         title: {
-//           text: "Values",
-//         },
-//       },
-//       series: [
-//         {
-//           name: "Values",
-//           data: Object.values(data),
-//         },
-//       ],
-//     });
-//   }
-
-//   document.getElementById("refresh").addEventListener("click", function () {
-//     localStorage.clear();
-//     location.reload();
-//   });
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const fileDropdown = document.getElementById("files");
   const keysDropdown = document.getElementById("keys");
@@ -392,6 +192,88 @@ document.addEventListener("DOMContentLoaded", function () {
     location.reload();
   });
 
+  // Function to change text based on selected language
+  // function changeLanguage() {
+  //   const language = document.getElementById("language").value;
+  //   const rashiDropdown = document.getElementById("files");
+  //   const rashiLabels = document.querySelectorAll(".rashi-label");
+
+  // const rashiTranslations = {
+  //   hi: {
+  //     mesh: "मेष",
+  //     vrishabh: "वृषभ",
+  //     mithun: "मिथुन",
+  //     kark: "कर्क",
+  //     simha: "सिंह",
+  //     kanya: "कन्या",
+  //     tula: "तुला",
+  //     vrishchik: "वृश्चिक",
+  //     dhanu: "धनु",
+  //     makar: "मकर",
+  //     kumbh: "कुंभ",
+  //     meen: "मीन",
+  //   },
+  //   mr: {
+  //     mesh: "मेष",
+  //     vrishabh: "वृषभ",
+  //     mithun: "मिथुन",
+  //     kark: "कर्क",
+  //     simha: "सिंह",
+  //     kanya: "कन्या",
+  //     tula: "तुला",
+  //     vrishchik: "वृश्चिक",
+  //     dhanu: "धनु",
+  //     makar: "मकर",
+  //     kumbh: "कुंभ",
+  //     meen: "मीन",
+  //   },
+  //   sa: {
+  //     mesh: "मेष",
+  //     vrishabh: "वृषभ",
+  //     mithun: "मिथुन",
+  //     kark: "कर्क",
+  //     simha: "सिंह",
+  //     kanya: "कन्या",
+  //     tula: "तुला",
+  //     vrishchik: "वृश्चिक",
+  //     dhanu: "धनु",
+  //     makar: "मकर",
+  //     kumbh: "कुंभ",
+  //     meen: "मीन",
+  //   },
+  // };
+
+  //   switch (language) {
+  //     case "hi":
+  //     case "mr":
+  //     case "sa":
+  //       // Change rashi names in dropdown
+  //       for (let i = 0; i < rashiDropdown.options.length; i++) {
+  //         const rashiName = rashiDropdown.options[i].value.split(".")[0];
+  //         if (rashiTranslations[language][rashiName]) {
+  //           rashiDropdown.options[i].text =
+  //             rashiTranslations[language][rashiName];
+  //         }
+  //       }
+  //       // Change rashi labels
+  //       rashiLabels.forEach((label) => {
+  //         label.textContent = "राशी चुनें :";
+  //       });
+  //       break;
+  //     default:
+  //       // Change rashi names in dropdown back to default
+  //       for (let i = 0; i < rashiDropdown.options.length; i++) {
+  //         const rashiName = rashiDropdown.options[i].value.split(".")[0];
+  //         rashiDropdown.options[i].text =
+  //           rashiName.charAt(0).toUpperCase() + rashiName.slice(1);
+  //       }
+  //       // Change rashi labels back to default
+  //       rashiLabels.forEach((label) => {
+  //         label.textContent = "Select Rashi :";
+  //       });
+  //   }
+  // }
+
   function changeLanguage() {
     const language = document.getElementById("language").value;
     const rashiDropdown = document.getElementById("files");
@@ -431,19 +313,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const rashiTranslations = {
       hi: {
-        "kark.json": "कर्क",
-        "dhanu.json": "धनु",
-        "kumbh.json": "कुंभ",
+        kark: "कर्क",
+        dhanu: "धनु",
+        kumbh: "कुंभ",
       },
       mr: {
-        "kark.json": "कर्क",
-        "dhanu.json": "धनु",
-        "kumbh.json": "कुंभ",
+        kark: "कर्क",
+        dhanu: "धनु",
+        kumbh: "कुंभ",
       },
       sa: {
-        "kark.json": "कर्क",
-        "dhanu.json": "धनु",
-        "kumbh.json": "कुंभ",
+        kark: "कर्क",
+        dhanu: "धनु",
+        kumbh: "कुंभ",
       },
     };
 
@@ -474,14 +356,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Change rashi names in dropdown
     for (let i = 0; i < rashiDropdown.options.length; i++) {
-      const rashiName = rashiDropdown.options[i].value;
+      const rashiName = rashiDropdown.options[i].value.split(".")[0];
       if (rashiTranslations[language][rashiName]) {
         rashiDropdown.options[i].text = rashiTranslations[language][rashiName];
       }
     }
-
-    // Trigger change event on fileDropdown to refresh keysDropdown and valuesDropdown
-    fileDropdown.dispatchEvent(new Event("change"));
   }
 
   // Call the function to set labels initially
